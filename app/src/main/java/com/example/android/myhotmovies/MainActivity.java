591d449110e5774f296ef42c4696f478abc3ac3a
tonyxie <tonyxie_0419@qq.com>
2017-12-12 23:20:40 +0800

    如下改变：          1、可以在电影详情页中查看预告片和评论了          2、可以收藏电影了          3、增加了菜单按钮-收藏的电影          4、优化了界面布局

--- app/src/main/java/com/example/android/myhotmovies/MainActivity.java
+++ app/src/main/java/com/example/android/myhotmovies/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
